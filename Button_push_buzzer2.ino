859b7b4cd097dd580f8add52df67798e9415467b
uncukonnen <34085625+uncukonnen@users.noreply.github.com>
2017-12-13 00:19:37 +0700

    Add files via upload

---
+++ Button_push_buzzer2.ino
@@ -0,0 +1,20 @@
+
+const int buttonPin = 4;    
+const int ledPin =  8;     
+int buzzer = 6;
+
+//int buttoPushCounter = 0;
+//int buttonState = 0;         
+//int lastButtonState = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(buzzer,OUTPUT);
+  
+  Serial.print(9600);
+}
+
+void loop() {
+
+ 
